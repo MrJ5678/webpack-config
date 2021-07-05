ff3eac672b75345e31fbe03f94cdd39fbabe465f
hhhhhq <Mr.j@tom.com>
2021-07-05 18:19:17 +0800

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss"
 import Recipes from "./Recipes"
+import facebook from "../images/undraw_lightbulb_moment_re_ulyo.png"
+import unDraw from "../images/undraw_Sync_files_re_ws4c.svg"
 
 function App() {
   return (
@@ -9,9 +11,10 @@ function App() {
         <section>
           <h1>Oh Hai, React</h1>
         </section>
+        <img src={facebook} alt="facebook" width="250" />
+        <img src={unDraw} alt="unDraw" width="250" />
+        <Recipes />
       </main>
-
-      <Recipes />
     </>
   )
 }
